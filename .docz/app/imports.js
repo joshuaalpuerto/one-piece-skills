388b02d18ad46965acb3cccf1b0e93d685781c70
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-08 14:08:37 +0800

    Ignore docz cached

--- .docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'app/components/Input/input.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "app-components-input-input" */ 'app/components/Input/input.mdx')
-}

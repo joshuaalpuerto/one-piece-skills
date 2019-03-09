e76ed6b39e3967e115a590bb3f1d9829be714146
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-09 20:40:57 +0800

    Add enzyme helper for testing

---
+++ app/utils/Enzyme/props.js
@@ -0,0 +1,10 @@
+export const INTL_SHAPE = {
+  formatDate: () => {},
+  formatTime: () => {},
+  formatRelative: () => {},
+  formatNumber: () => {},
+  formatPlural: () => {},
+  formatMessage: () => {},
+  formatHTMLMessage: () => {},
+  now: () => {}
+}

1c4470151c68a00daae09dfbdaf37e25f872c12c
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-09 20:27:48 +0800

    Apply component for UI

--- app/containers/SkillsPage/index.js
+++ app/containers/SkillsPage/index.js
@@ -1,23 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
+import { FormattedMessage } from 'react-intl'
 
 import injectReducer from 'utils/injectReducer'
+import H3 from 'components/H3'
 
+import Form from './Form'
+import Skills from './Skills'
 import reducer from './reducer'
+import messages from './messages'
+
+const Wrapper = styled.section`
+  margin: 20px 0;
+`
 
 /* eslint-disable react/prefer-stateless-function */
 export class SkillsPage extends React.PureComponent {
+  onSubmit = () => {}
+
   render() {
     return (
-      <article>
+      <Wrapper>
         <Helmet>
           <title>Add skills</title>
           <meta name="description" content="Add skills page" />
         </Helmet>
-        <div />
-      </article>
+        <H3>
+          <FormattedMessage {...messages.header} />
+        </H3>
+        <Form onSubmit={this.onSubmit} />
+        <Skills skills={[]} />
+      </Wrapper>
     )
   }
 }

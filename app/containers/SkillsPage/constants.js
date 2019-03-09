4d31bd50b5b498f754cf186c6338dbb2298b3cbf
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-09 19:25:41 +0800

    Add experience data

--- app/containers/SkillsPage/constants.js
+++ app/containers/SkillsPage/constants.js
@@ -1,3 +1,7 @@
+import experiences from 'data/experiences.json'
+
 export const GET_SKILLS = 'app/SkillsPage/GET_SKILLS'
 export const CREATE_SKILLS = 'app/SkillsPage/CREATE_SKILLS'
 export const RESULT_SKILLS = 'app/SkillsPage/RESULT_SKILLS'
+
+export const EXPERIENCES = experiences

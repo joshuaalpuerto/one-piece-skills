3bcf65a534885a565db6fdc77c51908395241faa
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-10 11:16:35 +0800

    Apply side-effect and and implement data for components

--- app/containers/SkillsPage/index.js
+++ app/containers/SkillsPage/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { FormattedMessage } from 'react-intl'
+import { createStructuredSelector } from 'reselect'
 
 import injectReducer from 'utils/injectReducer'
+import injectSaga from 'utils/injectSaga'
 import H3 from 'components/H3'
+import LoadingIndicator from 'components/LoadingIndicator'
 
 import Form from './Form'
 import Skills from './Skills'
 import reducer from './reducer'
+import saga from './saga'
 import messages from './messages'
+
+import { getSkills, createSkills } from './actions'
+import { selectSkills, selectSkillsLoader } from './selectors'
 
 const Wrapper = styled.section`
   margin: 20px 0;
@@ -19,9 +27,16 @@ const Wrapper = styled.section`
 
 /* eslint-disable react/prefer-stateless-function */
 export class SkillsPage extends React.PureComponent {
-  onSubmit = () => {}
+  componentDidMount() {
+    this.props.getSkills()
+  }
+
+  onSubmit = values => {
+    this.props.createSkills(values)
+  }
 
   render() {
+    const { skills, skillsLoader } = this.props
     return (
       <Wrapper>
         <Helmet>
@@ -32,22 +47,42 @@ export class SkillsPage extends React.PureComponent {
           <FormattedMessage {...messages.header} />
         </H3>
         <Form onSubmit={this.onSubmit} />
-        <Skills skills={[]} />
+        {skillsLoader ? <LoadingIndicator /> : <Skills skills={skills} />}
       </Wrapper>
     )
   }
 }
 
-SkillsPage.propTypes = {}
+SkillsPage.propTypes = {
+  getSkills: PropTypes.func.isRequired,
+  createSkills: PropTypes.func.isRequired,
+  skills: PropTypes.object.isRequired,
+  skillsLoader: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = createStructuredSelector({
+  skills: selectSkills(),
+  skillsLoader: selectSkillsLoader()
+})
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    getSkills: payload => dispatch(getSkills(payload)),
+    createSkills: payload => dispatch(createSkills(payload)),
+    dispatch
+  }
+}
 
 const withConnect = connect(
-  null,
-  null
+  mapStateToProps,
+  mapDispatchToProps
 )
 
 const withReducer = injectReducer({ key: 'skillsPage', reducer })
+const withSaga = injectSaga({ key: 'skillsPage', saga })
 
 export default compose(
   withReducer,
+  withSaga,
   withConnect
 )(SkillsPage)

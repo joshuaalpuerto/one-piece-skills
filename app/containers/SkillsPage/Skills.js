7453623dd46e560f3464be96454d257e7977c0a2
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-03-10 10:49:00 +0800

    Hide dependency to component

--- app/containers/SkillsPage/Skills.js
+++ app/containers/SkillsPage/Skills.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 import styled from 'styled-components'
 
+import { toJS } from 'utils/Immutable'
 import Card from 'components/Card'
 
 import messages from './messages'
@@ -42,4 +43,11 @@ Skills.propTypes = {
   skills: PropTypes.array
 }
 
-export default Skills
+/**
+ * We dont want dumb component
+ * to have dependencies  to immutable
+ * We also don't wnant to call`toJS` to object always since this will cause performance issue
+ * so we  wrap it to HOC
+ * issue: https://github.com/react-boilerplate/react-boilerplate/issues/2469
+ */
+export default toJS(Skills)
